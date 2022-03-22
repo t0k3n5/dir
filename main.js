29eb018eca2e6ed88458a879a640891a06887227
Animetrical <88539234+Animetrical@users.noreply.github.com>
2022-03-22 17:39:31 +0000

    hello world

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 /* main.js */
+
+
+/* A simple script which will get some basic data and return it using console.log('data'). */
+
+let username = ''
+let password = ''
+let email = ''
+
+function secret(string, key)
+{   
+    if (!typeof(string) == 'string') { return }
+    if (!typeof(key) == 'string')
+
+    var returnstring = ''
+
+    for (x = 1; x <= string.length; x++)
+    {
+        returnstring = returnstring + key
+    }
+
+    return returnstring
+}
+
+console.log('username: ' + username)
+console.log('password: ' + secret(password, '*'))
+console.log('email: ' + secret("@" + email.split("@")[2]), "*")
